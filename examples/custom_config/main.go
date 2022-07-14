7fcae37eab6aab70d7050bcd72d37124cb26cc24
Laky64 <35636667+Laky-64@users.noreply.github.com>
2022-07-14 14:30:45 +0200

    Updated Examples

--- examples/custom_config/main.go
+++ examples/custom_config/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/Squirrel-Network/gobotapi"
+	"github.com/Squirrel-Network/gobotapi/logger"
+	"log"
+	"os"
 	"time"
 )
 
@@ -12,12 +15,15 @@ func main() {
 		8080,        // Your bot API port
 		false,       // Your bot API protocol
 	)
-	client.PollingTimeout = time.Second * 10            // Timeout for polling
-	client.NoUpdates = true                             // If true, the bot will not receive updates
-	client.DownloadRefreshRate = time.Millisecond * 200 // Rate of download refresh time (in milliseconds)
-	client.AllowedUpdates = []string{"message"}         // Allowed updates
-	client.Beta = true                                  // If true, the bot will use the beta API
-	client.SleepThreshold = 10                          // Sleep threshold, by default is 10
+	client.PollingTimeout = time.Second * 10                    // Timeout for polling
+	client.NoUpdates = true                                     // If true, the bot will not receive updates
+	client.DownloadRefreshRate = time.Millisecond * 200         // Rate of download refresh time (in milliseconds)
+	client.AllowedUpdates = []string{"message"}                 // Allowed updates
+	client.Beta = true                                          // If true, the bot will use the beta API
+	client.SleepThreshold = 10                                  // Sleep threshold, by default is 10
+	client.LoggingLevel = logger.Debug                          // Logging level
+	client.LoggerColorful = true                                // If true, the logger will use colorful output
+	client.LoggerWriter = log.New(os.Stdout, "", log.LstdFlags) // If you want to use a custom writer, you can set it here
 	// Start and idle the bot
 	_ = client.Run()
 }
